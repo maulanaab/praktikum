6fa2eac19e899f316c623d4b23f69ffc89ef37ac
maulanaab <anugrahmagelang06@gmail.com>
2026-08-31 16:43:17 +0700

    Add files via upload

---
+++ literal_data.py
@@ -0,0 +1,38 @@
+#Tugas No 1
+nama = "Maulana Abimanyu Pradana"
+umur = 20
+berat = 43.9
+
+print("Nama = ", nama, type(nama))
+print("Umur = ", umur, type(umur))
+print("Berat = ", berat, type(berat))
+print(" ")
+#Tugas No 2
+angka_string = "123"
+angka_float = 45.67
+angka_integer = 89
+
+#1. Konversi angka_string menjadi integer
+angka_integer = int(angka_string)
+print("Hasil konversi = ", angka_integer, type(angka_integer))
+
+#2. Konversi angka_float menjadi integer
+angka_integer = int(angka_float)
+print("Hasil konversi = ", angka_integer, type(angka_integer))
+
+#3. Konversi angka_integer menjadi float
+angka_float = float(angka_integer)
+print("Hasil konversi = ", angka_float, type(angka_float))
+
+#4. Konversi angka_integer menjadi string
+angka_string = str(angka_integer)
+print("Hasil konversi = ", angka_string, type(angka_string))
+print(" ")
+#Tugas No 3
+usia = int(input("Masukan Usia = "))
+tinggi_badan = float(input("Masukan Tinggi Badan = "))
+nama = str(input("Masukan Nama = "))
+print(" ")
+print("Usia = ", usia, type(usia))
+print("Tinggi Badan = ", tinggi_badan, type(tinggi_badan))
+print("Nama = ", nama, type(nama))
